71fd05ef33db6b16595ea26a8accbc2641cbc92f
Aemon <bot960528@gmail.com>
2021-03-09 14:12:33 +0800

    edit
    1. 修改了库的名称；
    2. 修改了配置。

--- public/static/config.js
+++ public/static/config.js
@@ -1,3 +1,14 @@
-window.api={
-    webjx:"http://www.baidu.com"
+window.api = {
+    userName: 'chancel',
+    password: '123456',
+    previewData: {
+        web: [
+            {name: "家系", url: "http://www.baidu.com", logo: ""},
+            {name: "家系", url: "http://www.baidu.com", logo: ""},
+            {name: "家系", url: "http://www.baidu.com", logo: ""},
+            {name: "家系", url: "http://www.baidu.com", logo: ""}
+        ],
+        app: [],
+        exe: []
+    }
 }
